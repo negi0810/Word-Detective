eb458d6e9d3a9f54d36de782841d35823e3b581e
USK314 <usk31101021@gmail.com>
2022-05-20 23:26:53 +0900

    fix: firebase.py

--- firebase.py
+++ firebase.py
@@ -1,32 +1,55 @@
+# import os
+# import firebase_admin
+# from firebase_admin import credentials
+# from firebase_admin import storage
+# from firebase_admin import firestore
+
+# cred_file_path = ""
+
+# tmp_cred_file_path = "tmp/cred.json"
+
+# is_local = os.path.exists("cert.json")
+
+# if is_local:
+#     cred_file_path = "cert.json"
+# else:
+#     json = os.environ.get("cert_json")
+#     with open(tmp_cred_file_path, mode='w') as f:
+#         f.write(json)
+#     cred_file_path = tmp_cred_file_path
+
+# from google.oauth2 import service_account
+
+# cred = credentials.Certificate(cred_file_path)
+
+# if not is_local:
+#     os.remove(tmp_cred_file_path)
+
+# firebase_app = firebase_admin.initialize_app(cred, {
+#     'storageBucket': 'word-detective-7ac3b.appspot.com',
+#     'projectId': 'word-detective-7ac3b',
+# })
+
+
+# bucket = storage.bucket()
+
+# db = firestore.client()
+
 import os
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import storage
 from firebase_admin import firestore
 
+tmp_cred_file_path = "cred.json"
 cred_file_path = ""
 
-tmp_cred_file_path = "cred.json"
-
-is_local = os.path.exists("cert.json")
-
-if is_local:
-    cred_file_path = "cert.json"
-else:
-    json = os.environ.get("cert_json")
-    with open(tmp_cred_file_path, mode='w') as f:
-        f.write(json)
-    cred_file_path = tmp_cred_file_path
-
-from google.oauth2 import service_account
-
-vision_api_cred = service_account.Credentials.from_service_account_file(cred_file_path)
-
+cert_json_str = os.environ.get("cert_json")
+open(tmp_cred_file_path, mode='w').write(cert_json_str)
+cred_file_path = tmp_cred_file_path
 
 cred = credentials.Certificate(cred_file_path)
-
-if not is_local:
-    os.remove(tmp_cred_file_path)
+os.remove(tmp_cred_file_path)
 
 firebase_app = firebase_admin.initialize_app(cred, {
     'storageBucket': 'word-detective-7ac3b.appspot.com',
