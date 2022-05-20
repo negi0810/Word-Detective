356ede830cddf65bea50caeda97a9906aca461fe
USK314 <usk31101021@gmail.com>
2022-05-21 00:01:20 +0900

    fix: a little

--- firebase.py
+++ firebase.py
@@ -49,7 +49,7 @@ f = open(cred_file_path, mode='w')
 f.write(cert_json_str)
 f.close()
 cred = credentials.Certificate(cred_file_path)
-
+print("test")
 # os.remove(cred_file_path)
 
 firebase_app = firebase_admin.initialize_app(cred, {
